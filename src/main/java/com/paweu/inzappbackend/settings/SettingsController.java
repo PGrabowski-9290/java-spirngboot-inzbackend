c96827efecf57edfd461e3e69956990d21f5109d
PGrabowski-9290 <pawel_grabowski99@outlook.com>
2023-04-29 18:41:09 +0200

    Add get global settings

--- src/main/java/com/paweu/inzappbackend/settings/SettingsController.java
+++ src/main/java/com/paweu/inzappbackend/settings/SettingsController.java
@@ -1,2 +1,26 @@
-package com.paweu.inzappbackend.settings.service;public class SettingsController {
+package com.paweu.inzappbackend.settings;
+
+import com.paweu.inzappbackend.db.models.Settings;
+import com.paweu.inzappbackend.models.ReqResp.Resp;
+import com.paweu.inzappbackend.settings.service.SettingsService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+@RestController
+@RequestMapping("/settings")
+public class SettingsController {
+
+    private final SettingsService settingsService;
+
+    public SettingsController(SettingsService settingsService) {
+        this.settingsService = settingsService;
+    }
+
+    @GetMapping("/global")
+    public Mono<ResponseEntity<Resp<Settings>>> getGlobal(){
+        return settingsService.getSettingsGlobal();
+    }
 }
--- src/main/java/com/paweu/inzappbackend/settings/service/SettingsService.java
+++ src/main/java/com/paweu/inzappbackend/settings/service/SettingsService.java
@@ -1,2 +1,28 @@
-package com.paweu.inzappbackend.settings.service;public class SettingsService {
+package com.paweu.inzappbackend.settings.service;
+
+import com.paweu.inzappbackend.db.ISettingsRepository;
+import com.paweu.inzappbackend.db.models.Settings;
+import com.paweu.inzappbackend.models.ReqResp.Resp;
+import com.paweu.inzappbackend.models.exception.ResponseExceptionModel;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Mono;
+
+@Service
+public class SettingsService {
+    private final ISettingsRepository settingsRepository;
+
+    public SettingsService(ISettingsRepository settingsRepository) {
+        this.settingsRepository = settingsRepository;
+    }
+
+    public Mono<ResponseEntity<Resp<Settings>>>  getSettingsGlobal(){
+        return settingsRepository.findFirstById(0)
+                .onErrorResume(throwable -> Mono.error(new ResponseExceptionModel("Cos nie pyklo",400)))
+                .switchIfEmpty(Mono.error(new ResponseExceptionModel("nie ma",404)))
+                .flatMap(settings -> {
+                    System.out.println(settings == null);
+                    return Mono.just(ResponseEntity.ok().body(new Resp<Settings>("Success", settings)));
+                });
+    }
 }
